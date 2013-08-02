c105a4abdb94362ebff521a26d7f4f15160cdc18
Dhruv Bansal <dhruv@ph.utexas.edu>
2013-08-02 11:16:14 -0500

    set vayacondios options as wukong options

--- lib/wukong-vayacondios.rb
+++ lib/wukong-vayacondios.rb
@@ -13,13 +13,14 @@ module Wukong
     # @param [Configliere::Param] settings
     # @return [Configliere::Param] the newly configured settings
     def self.configure settings, program
-      settings.define(:vcd,             :description => "Interact with Vayacondios server", type: :boolean, default: false)
-      settings.define(:vcd_host,        :description => "Host for Vayacondios server", default: 'localhost')
-      settings.define(:vcd_port,        :description => "Port for Vayacondios server", type: Integer, default: 9000)
-      settings.define(:organization,    :description => "Vayacondios organization")
+      settings.define(:vcd,             :wukong => true, :description => "Interact with Vayacondios server", type: :boolean, default: false)
+      settings.define(:vcd_host,        :wukong => true, :description => "Host for Vayacondios server", default: 'localhost')
+      settings.define(:vcd_port,        :wukong => true, :description => "Port for Vayacondios server", type: Integer, default: 9000)
+      settings.define(:organization,    :wukong => true, :description => "Vayacondios organization")
     end
 
-    # Boot Wukong::Vayacondios with the given `settings` in the given `dir`.
+    # Boot Wukong::Vayacondios with the given `settings` in the given
+    # `dir`.
     #
     # @param [Configliere::Param] settings
     # @param [String] root
